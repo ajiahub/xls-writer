53f04bdbfb93c5c364d75ccb38b028b719937f33
ligang <568621896@qq.com>
2022-04-14 17:29:47 +0800

    exception

--- src/Import.php
+++ src/Import.php
@@ -13,20 +13,28 @@ class Import extends ImportAbstract
 {
     private string $fileName;
     private array $config;
+    private string $file;
 
     public function __construct($file)
     {
+        $this->file = $file;
         $this->fileName = basename($file);
         $this->config = ['path'=>dirname($file)];
     }
 
     public function getSheet()
     {
+        if(!file_exists($this->file)){
+            throw new \Exception('File'.$this->file.' Not Found.');
+        }
         return (new XlsWriter($this->config))->instance()->openFile($this->fileName)->openSheet()->getSheetData();
     }
 
     public function instance()
     {
+        if(!file_exists($this->file)){
+            throw new \Exception('File'.$this->file.' Not Found.');
+        }
         $xlsWriter = (new XlsWriter($this->config))->instance()->openFile($this->fileName)->openSheet();
         return $xlsWriter->excel;
     }
